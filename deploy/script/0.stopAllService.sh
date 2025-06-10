145470c4793e89752cdb0ebebab0cb70fedcde5a
jin7942 <infomer.jin@gmail.com>
2025-06-11 01:34:40 +0900

    fix typo

--- deploy/script/0.stopAllService.sh
+++ deploy/script/0.stopAllService.sh
@@ -11,19 +11,19 @@ sleep 5
 
 # dockwatch
 echo "Stopping dockwatch..."
-docker compose -f /srv/deploy/nginx-config/dockwatch/docker-compose.yml down 
+docker compose -f /srv/deploy/nginx-config/deploy/dockwatch/docker-compose.yml down 
 echo "dockwatch stopped."
 sleep 5
 
 # usedmarket
 echo "Stopping usedmarket..."
-docker compose -f /srv/deploy/nginx-config/usedmarket/docker-compose.yml down 
+docker compose -f /srv/deploy/nginx-config/deploy/usedmarket/docker-compose.yml down 
 echo "usedmarket stopped."
 sleep 5
 
 # ray-auto-deploy-server
 echo "Stopping ray-auto-deploy-server..."
-docker compose -f /srv/deploy/nginx-config/ray-auto-deploy-server/docker-compose.yml down 
+docker compose -f /srv/deploy/nginx-config/deploy/ray-auto-deploy-server/docker-compose.yml down 
 echo "ray-auto-deploy-server stopped."
 sleep 5
 
